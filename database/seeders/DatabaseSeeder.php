64745e96194ce0fd76d1e42eb1207cefcd46203e
Hajj Employment System <developer@hajj-employment.com>
2025-07-17 10:36:45 +0300

    إضافة بيانات تجريبية وتحديثات للوظائف
    - إضافة MeccaJobsSeeder مع 5 وظائف في مكة (بدون تسجيل) و 1 وظيفة في المدينة (بتسجيل)
    - تحديث DatabaseSeeder لتشغيل البيانات التجريبية
    - إضافة migration لتحديث الوظائف الموجودة بالحقول الجديدة
    - ضمان عمل النظام المزدوج بشكل صحيح للاختبار

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,7 @@ class DatabaseSeeder extends Seeder
             RolesAndPermissionsSeeder::class,
             AdminUserSeeder::class,
             HajjJobsSeeder::class,
+            MeccaJobsSeeder::class,
         ]);
     }
 }
